03692bd9878f3d556bed0acb8c2d66a95f290804
Rahul Deshmukh <rahuld@hotmail.com>
2020-07-03 11:58:52 -0400

    1. Added project 2. Added NuGet dependency 3. Added Utility class

---
+++ src/AppBlocks.Autofac/Common/Utility.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace AppBlocks.Autofac.Common
+{
+    public static class Utility
+    {
+        public static bool IsAssembly(string assemblyPath)
+        {
+            try
+            {
+                // Attempt to resolve the assembly
+                var assembly = AssemblyName.GetAssemblyName(assemblyPath);
+                return true;
+            }
+            catch { }
+            return false;
+        }
+    }
+}
